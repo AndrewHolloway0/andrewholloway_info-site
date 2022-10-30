8cd6074d6741e11729b9e2ac48274868fb960b7e
TwiddlesPenguin <andrew.k.holloway+git@hotmail.com>
2022-10-30 23:34:13 +0800

    page v2
    mobile first - 70% done

--- 2.php
+++ 2.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Andrew Holloway">
     <meta name="viewport" content="width=device-width,user-scalable=no" />
     <link rel="icon" href="/favicon.png?v=0002" />
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="main2.css" />
     <link rel="stylesheet" href="home.css" />
     <script>
         
@@ -16,91 +16,148 @@
 </head>
 
 <body>
-    <?php require("./includes/header.php"); ?>
-    <div class="section" id="intro">
-        <div class="container">
-            <div id="introimg"></div>
-            <div id="introtxt">
-                <p id="introtxttitle">
-                    <span>I'm</span>
-                    <span>Andrew</span>
-                    <span>Holloway</span>
-                </p>
-                <p>
-                    Experienced IT professional with a demonstrated history of professional
-                    communication and advanced computer networking skills.
-                </p>
-                <div id="introbuttons">
-                    <button>DOWNLOAD CV</button>
-                    <span>or alternatively, read this site</span>
-                </div>
-            </div>
-        </div>
-        <div id="socialbuttons"></div>
+    <div id="bodybg">
+        <div class="bgball size1"></div>
+        <div class="bgball size2"></div>
+        <div class="bgball size3"></div>
+        <div class="bgball size4"></div>
+        <div class="bgball size5"></div>
     </div>
-    <div class="section" id="summary">
-        <div class="container">
-            <h2 class="sectiontitle">Executive Summary</h2>
+    <?php require("./includes/header.php"); ?>
+    <main>
+        <section id="main">
+            <div id="ahollowaytxt">
+                <h1><span class="clr-accent">Andrew</span> Holloway</h1>
+                <p>IT PROFESSIONAL AND ENTHUSIAST</p>
+            </div>
+            <div id="summary" class="box box-highlighted">
+                <h2>Executive Summary</h2>
+                <p>
+                    I am a motivated and experienced individual with a demonstrated history working 
+                    professionally and efficiency in fast pasted environments. I am highly skilled in 
+                    advanced computer networking, team leadership, and programming languages like 
+                    Powershell, PHP, HTML and Javascript. I take pride in my customer-focused, work 
+                    ethic and ability to plan, document and communicate tasks for future repeatability 
+                    and cohesive knowledge across a team.
+                </p>
+            </div>
+        </section>
+    </main>
+    <section id="experience" class="purp-gradient-bg">
+        <div class="header">
+            <h2>Experience & Roles</h2>
             <p>
-                Andrew Holloway is a motivated and experienced individual with a demonstrated
-                history of professional working and efficiency in fast pasted environments.
-                Holloway is skilled in advanced computer networking, team leadership, and
-                programming languages like Powershell, PHP, HTML and Javascript. He is a
-                strong customer-focused professional with a strong work ethic and takes pride
-                in planning, documenting and communicating tasks for future repeatability
-                and cohesive knowledge across a team.
+                All of my previous roles provided me helpful experience in handling pressure,
+                time, and communication. Below, I show my growth, click for more info.
             </p>
         </div>
-    </div>
-    <div class="section" id="experience">
-        <div class="container">
-            <h2 class="sectiontitle">Experience & Roles</h2>
-            <p class="sectionexplain"></p>
-            <div class="timeline"></div>
-            <div class="modules">
-                <div id="trsmod">
-                    <div class="modheader"></div>
-                    <div class="moddesc"></div>
-                    <div class="modskills">
-                        <div class="softskills"></div>
-                        <div class="hardskills"></div>
-                    </div>
-                </div>
-                <div id="sccmod">
-                    <div class="modheader"></div>
-                    <div class="moddesc"></div>
-                    <div class="modskills">
-                        <div class="softskills"></div>
-                        <div class="hardskills"></div>
-                    </div>
-                </div>
+        <div class="content">
+            <div class="box box-highlighted">
+                <div id="scc" class="box-image"></div>
+                <div class="box-title heading">Seton Catholic College</div>
+                <div class="box-desc">ICT Officer</div>
+            </div>
+            <div class="box">
+                <div id="trs" class="box-image"></div>
+                <div class="box-title heading">The Reject Shop</div>
+                <div class="box-desc">Team Leader</div>
             </div>
         </div>
-    </div>
-    <div class="section" id="skills">
-        <div class="container">
-            <h2 class="sectiontitle">Skills & Certifications</h2>
-            
+    </section>
+    <section id="skills">
+        <div class="header">
+            <h2>Skills & Certifications</h2>
+            <p>
+                I've always had a passion to learn and grow
+                my knowledge. In the time I've been working,
+                I've also been studying. Here are some of
+                the certifications and skills I've acquired.
+            </p>
         </div>
-    </div>
-    <div class="section" id="education">
-        <div class="container">
-            <h2 class="sectiontitle">Education</h2>
-            
+        <div class="content">
+            <div class="box">
+                    <div class="box-image"></div>
+                    <div class="box-title heading">Diploma</div>
+                    <div class="box-desc">IT Networking</div>
+                </div>
+                <div class="box">
+                    <div class="box-image"></div>
+                    <div class="box-title heading">Certificate IV</div>
+                    <div class="box-desc">Business</div>
+                </div>
+                <div class="box">
+                    <div class="box-image"></div>
+                    <div class="box-title heading">Certification</div>
+                    <div class="box-desc">AWS Architect</div>
+                </div>
+            </div>
+        <a href="#" class="btn">LEARN MORE</a>
+    </section>
+    <section id="education" class="purp-gradient-bg">
+        <div class="header">
+            <h2>Education History</h2>
+            <p>
+                As and example of my passion and drive, during my final year of high school I was also
+                studying Cyber Security and Music Industry at South Metropolitan TAFE.
+            </p>
         </div>
-    </div>
-    <div class="section" id="portfolio">
-        <div class="container">
-            <h2 class="sectiontitle">Portfolios & Projects</h2>
-            
+        <div class="content">
+            <div class="box box-highlighted">
+                <div class="box-image"></div>
+                <div class="box-title heading">Diploma</div>
+                <div class="box-desc">IT Networking</div>
+            </div>
+            <div class="box">
+                <div class="box-image"></div>
+                <div class="box-title heading">Certificate IV</div>
+                <div class="box-desc">Business</div>
+            </div>
+            <div class="box">
+                <div class="box-image"></div>
+                <div class="box-title heading">Certificate II</div>
+                <div class="box-desc">Music Industry</div>
+            </div>
         </div>
-    </div>
-    <div class="section" id="contact">
-        <div class="container">
-            <h2 class="sectiontitle">Contact</h2>
-            
+    </section>
+    <section id="portfolios">
+        <div class="header">
+            <h2>Portfolios & Projects</h2>
+            <p>
+                I spend my extra time doing things I love, 
+                which includes creating music, programming
+                and much more. Here I've displayed some 
+                of my most interesting and best projects.
+            </p>
         </div>
-    </div>
+        <div class="content">
+            <div class="box">
+                <div class="box-image"></div>
+                <div class="box-title heading">Music</div>
+            </div>
+            <div class="box">
+                <div class="box-image"></div>
+                <div class="box-title heading">Programming</div>
+            </div>
+            <div class="box">
+                <div class="box-image"></div>
+                <div class="box-title heading">Websites</div>
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <div class="header">
+            <h2>Contact</h2>
+            <p>
+                If your interested in contacting me, feel free below. I'm always looking for new
+                opportunities, and if you think your team could use someone like me, shoot me a
+                message. Chat soon!
+            </p>
+        </div>
+        <div class="btn-container">
+            <a href="#" class="btn">Business Inquiry</a>
+            <a href="#" class="btn">LinkedIn</a>
+        </div>
+    </section>
     <?php require("./includes/footer.php"); ?>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,29 @@
-<div id="headscollfix"></div>
+<div id="head-scollfix"></div>
 <header>
-    <div class="container">
-        <div id="hgrid">
-            <div id="hlogo">
-                <img src="/media/icons/ajh.svg"/>
+    <div class="wrapper">
+        <div id="head-top">
+            <div class="head-socials">
+                <img src="/media/icons/email.svg" alt="email"/>
+                <img src="/media/icons/linkedin.svg" alt="linkedin"/>
             </div>
-            <nav id="hnav">
-                <a href="#summary">summary</a>
-                <a href="#experience">experience</a>
-                <a href="#skills">skills</a>
-                <a href="#education">education</a>
-                <a href="#portfolios">portfolios</a>
-                <a href="#contact">contact</a>
-            </nav>
+            <div id="head-logo"></div>
+            <div id="head-mobnav">
+                <button class="hamburger hamburger--collapse" type="button">
+                    <span class="hamburger-box">
+                        <span  class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
         </div>
+        <nav id="head-bottom">
+            <ul>
+                <li><a href="#summary">summary</a></li>
+                <li><a href="#experience">experience</a></li>
+                <li><a href="#skills">skills</a></li>
+                <li><a href="#education">education</a></li>
+                <li><a href="#portfolios">portfolios</a></li>
+                <li><a href="#contact">contact</a></li>
+            </ul>
+        </nav>
     </div>
 </header>
